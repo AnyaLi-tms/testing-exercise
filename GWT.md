93fe3ad55b0081e68ec391274c8ccedbaf235b00
LIAN18 <lian18@oocl.com>
2025-08-22 13:29:41 +0800

    feat(AccountService): add test methods

---
+++ GWT.md
@@ -0,0 +1,18 @@
+1. 测试第一个if (amount随便)
+   1. 两个都null
+   2. 前null
+   3. 后null
+   4. ~~两个都不为空~~
+2. 测试第二个if (两个都不为空)
+   1. amount是一个负数
+   2. amount是0
+   3. ~~amount是一个正数~~
+   
+3. 测试withdraw (amount是一个正数)
+   1. 第二个if的测试
+   2. balance 小于 amount
+   3. balance 等于 amount
+   4. balance 大于 amount
+4. 测试deposit
+   1. 第二个if的测试
+   2. account to的balance
--- src/test/java/AccountServiceTest.java
+++ src/test/java/AccountServiceTest.java
@@ -1,2 +1,126 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class AccountServiceTest {
+    @Test
+    void test_two_account_null() {
+        // Given
+        Account from = null;
+        Account to = null;
+        double amount = 0;
+        // When
+        TransferService transferService = new TransferService();
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> transferService.transfer(from, to, amount));
+        // Then
+        assertEquals("Accounts cannot be null", exception.getMessage());
+    }
+
+    @Test
+    void test_from_account_null() {
+        // Given
+        Account from = null;
+        Account to = new Account("123", 2);
+        double amount = 0;
+        // When
+        TransferService transferService = new TransferService();
+        // Then
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> transferService.transfer(from, to, amount));
+        assertEquals("Accounts cannot be null", exception.getMessage());
+    }
+
+    @Test
+    void test_to_account_null() {
+        // Given
+        Account from = new Account("123", 2);
+        Account to = null;
+        double amount = 0;
+        // When
+        TransferService transferService = new TransferService();
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> transferService.transfer(from, to, amount));
+        // Then
+        assertEquals("Accounts cannot be null", exception.getMessage());
+    }
+
+    @Test
+    void test_amount_negative() {
+        // Given
+        Account from = new Account("123", 2);
+        Account to = new Account("1234", 3);
+        double amount = -1;
+        // When
+        TransferService transferService = new TransferService();
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> transferService.transfer(from, to, amount));
+        // Then
+        assertEquals("Transfer amount must be positive", exception.getMessage());
+    }
+
+    @Test
+    void test_amount_zero() {
+        // Given
+        Account from = new Account("123", 2);
+        Account to = new Account("1234", 3);
+        double amount = 0;
+        // When
+        TransferService transferService = new TransferService();
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> transferService.transfer(from, to, amount));
+        // Then
+        assertEquals("Transfer amount must be positive", exception.getMessage());
+    }
+
+    @Test
+    void test_from_withdraw_balance_less_amount() {
+        // Given
+        Account from = new Account("123", 0);
+        Account to = new Account("1234", 3);
+        double amount = 1;
+        // When
+        TransferService transferService = new TransferService();
+        IllegalStateException exception = assertThrows(IllegalStateException.class, () -> transferService.transfer(from, to, amount));
+        // Then
+        assertEquals("Insufficient balance", exception.getMessage());
+    }
+
+    @Test
+    void test_from_withdraw_balance_equal_amount() {
+        // Given
+        Account from = new Account("123", 1);
+        Account to = new Account("1234", 3);
+        double amount = 1;
+        // When
+        TransferService transferService = new TransferService();
+        transferService.transfer(from, to, amount);
+        // Then
+        assertEquals(0, from.getBalance());
+    }
+
+    @Test
+    void test_from_withdraw_balance_more_amount() {
+        // Given
+        Account from = new Account("123", 2);
+        Account to = new Account("1234", 3);
+        double amount = 1;
+        // When
+        TransferService transferService = new TransferService();
+        transferService.transfer(from, to, amount);
+        // Then
+        assertEquals(1, from.getBalance());
+        assertEquals(4, to.getBalance());
+    }
+
+    @Test
+    void test_to_deposit() {
+        // Given
+        Account from = new Account("123", 2);
+        Account to = new Account("1234", 3);
+        double amount = 1;
+        // When
+        TransferService transferService = new TransferService();
+        transferService.transfer(from, to, amount);
+        // Then
+        assertEquals(1, from.getBalance());
+        assertEquals(4, to.getBalance());
+    }
+
 }
